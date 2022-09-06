f84d925a3e0ab66c0d08d969739ab007fb93837f
peppino <peppinocasula@yahoo.it>
2022-09-06 23:34:46 +0100

    mvp

---
+++ src/main/java/Library.java
@@ -0,0 +1,28 @@
+import java.awt.print.Book;
+import java.util.ArrayList;
+
+public class Library {
+
+    private String name;
+    private ArrayList<Books> collection;
+    private int capacity;
+
+    public Library(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+        this.collection = new ArrayList<Books>();
+    }
+
+    public int countBooks(){
+        return this.collection.size();
+    }
+
+    public String addBook(Books book){
+        if (this.collection.size() < this.capacity){
+            this.collection.add(book);
+            return "Book Added";
+        } else {
+            return "Stock is full, the book can't be added!";
+        }
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,46 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    Books book1;
+    Books book2;
+    Books book3;
+    Books book4;
+    Books book5;
+    Library publicLibrary;
+    Library myReadings;
+
+    @Before
+    public void setUp(){
+        book1 = new Books("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "Fiction");
+        book2 = new Books("The roaches have no king", "Daniel Evan Weiss", "Fiction");
+        book3 = new Books("Bad Pharma", "Ben Goldacre", "Journalism");
+        book4 = new Books("The Zen and the art of walking on the water", "Chris Jesus", "Biography");
+        book5 = new Books("The 100 greatest Devil's Grip players of all times and their techniques", "Peppino Casula", "Sport");
+        publicLibrary = new Library("Public Library", 4);
+        myReadings = new Library("My readings", 2);
+        publicLibrary.addBook(book1);
+        publicLibrary.addBook(book2);
+        publicLibrary.addBook(book3);
+    }
+
+    @Test
+    public void canCountBooks(){
+        assertEquals(3, publicLibrary.countBooks());
+    }
+
+    @Test
+    public void canAddBook(){
+        publicLibrary.addBook(book4);
+        assertEquals(4, publicLibrary.countBooks());
+    }
+
+    @Test
+    public void canCheckStockFull(){
+        publicLibrary.addBook(book4);
+        assertEquals("Stock is full, the book can't be added!", publicLibrary.addBook(book5));
+    }
+}
